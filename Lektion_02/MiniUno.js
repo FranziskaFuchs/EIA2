b0da5a1592b84f318f6d8694fae4cd6d8bfa3df8
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2020-10-23 14:03:52 +0200

    1

---
+++ Lektion_02/MiniUno.js
@@ -0,0 +1,6 @@
+window.onload;
+function () {
+    var anzahl = prompt("Geben sie eine Kartenanzahl ein: ");
+    document.getElementById("Anzahl").innerHTML = "Anzahl + anzahl ";
+}
+;
---
+++ Lektion_02/MiniUno.ts
@@ -0,0 +1,4 @@
+window.onload function(){ 
+    let anzahl = prompt ("Geben sie eine Kartenanzahl ein: ");
+    document.getElementById("Anzahl").innerHTML="Anzahl + anzahl ";
+};
