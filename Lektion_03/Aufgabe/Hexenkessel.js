01878c695a74239ef3be4fb5011c6438bb5077d3
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2020-11-06 11:41:32 +0100

    1

--- Lektion_03/Aufgabe/Hexenkessel.js
+++ Lektion_03/Aufgabe/Hexenkessel.js
@@ -1,20 +1,59 @@
-var L04_Hexenkessel;
-(function (L04_Hexenkessel) {
+"use strict";
+var L03_Hexenkessel;
+(function (L03_Hexenkessel) {
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         console.log("Start");
-        var form = document.querySelector("div#form");
+        let form = document.querySelector("div#form");
         form.addEventListener("change", handleChange);
     }
     function handleChange(_event) {
-        var order = document.querySelector("div#order");
-        order.innerHTML = " ";
-        var formData = new FormData(document.forms[0]);
-        for (var _i = 0, formData_1 = formData; _i < formData_1.length; _i++) {
-            var entry = formData_1[_i];
-            var item = document.querySelector("[value='" + entry[1] + "']");
-            var price = Number(item.getAttribute("price"));
-            order.innerHTML += item.name + "  € " + price;
+        let dragon = document.querySelector("#dragon");
+        let witch = document.querySelector("#witch");
+        let raven = document.querySelector("#raven");
+        let liquid = document.querySelector("#liquid");
+        let medium = document.querySelector("#medium");
+        let tough = document.querySelector("#tough");
+        let recipe = document.querySelector("#display");
+        recipe.innerHTML = "";
+        let formData = new FormData(document.forms[0]);
+        for (let entry of formData) {
+            switch (entry[0]) {
+                case "potion":
+                    if (entry[1] != "") {
+                        recipe.innerHTML += "Name: " + entry[1] + "<br>";
+                    }
+                    break;
+                case "descript":
+                    if (entry[1] != "") {
+                        recipe.innerHTML += "Description: " + entry[1] + "<br>";
+                    }
+                    break;
+                case "effect":
+                    if (entry[1] != "") {
+                        recipe.innerHTML += "Effect: " + entry[1] + "<br>";
+                    }
+                    break;
+                case "time":
+                    if (entry[1] != "") {
+                        recipe.innerHTML += "Duration: " + entry[1] + " min" + "<br>";
+                    }
+                    break;
+                case "ingredient":
+                    if (entry[1] != "") {
+                        recipe.innerHTML += "Ingredient: " + entry[1] + "<br>";
+                    }
+                    break;
+                case "amountDragon":
+                case "amountWitch":
+                case "amountRaven":
+                case "temperature":
+                    if (entry[1] != "") {
+                        recipe.innerHTML += "Temperature: " + entry[1] + " °C" + "<br>";
+                    }
+                    break;
+            }
         }
     }
-})(L04_Hexenkessel || (L04_Hexenkessel = {}));
+})(L03_Hexenkessel || (L03_Hexenkessel = {}));
+//# sourceMappingURL=script.js.map
