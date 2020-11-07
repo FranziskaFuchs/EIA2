80424bec3b9c87f16092bbd9091da687415c615d
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2020-11-07 23:45:48 +0100

    1

--- Lektion_03/Aufgabe/Hexenkessel.js
+++ Lektion_03/Aufgabe/Hexenkessel.js
@@ -1,59 +1,30 @@
-"use strict";
-var L03_Hexenkessel;
-(function (L03_Hexenkessel) {
+var L04_Hexenkessel;
+(function (L04_Hexenkessel) {
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         console.log("Start");
-        let form = document.querySelector("div#form");
-        form.addEventListener("change", handleChange);
+        var form = document.querySelector("div#form");
+        var order = document.querySelector("div#order");
+        order.addEventListener("change", handleChange);
     }
     function handleChange(_event) {
-        let dragon = document.querySelector("#dragon");
-        let witch = document.querySelector("#witch");
-        let raven = document.querySelector("#raven");
-        let liquid = document.querySelector("#liquid");
-        let medium = document.querySelector("#medium");
-        let tough = document.querySelector("#tough");
-        let recipe = document.querySelector("#display");
-        recipe.innerHTML = "";
-        let formData = new FormData(document.forms[0]);
-        for (let entry of formData) {
-            switch (entry[0]) {
-                case "potion":
-                    if (entry[1] != "") {
-                        recipe.innerHTML += "Name: " + entry[1] + "<br>";
-                    }
-                    break;
-                case "descript":
-                    if (entry[1] != "") {
-                        recipe.innerHTML += "Description: " + entry[1] + "<br>";
-                    }
-                    break;
-                case "effect":
-                    if (entry[1] != "") {
-                        recipe.innerHTML += "Effect: " + entry[1] + "<br>";
-                    }
-                    break;
-                case "time":
-                    if (entry[1] != "") {
-                        recipe.innerHTML += "Duration: " + entry[1] + " min" + "<br>";
-                    }
-                    break;
-                case "ingredient":
-                    if (entry[1] != "") {
-                        recipe.innerHTML += "Ingredient: " + entry[1] + "<br>";
-                    }
-                    break;
-                case "amountDragon":
-                case "amountWitch":
-                case "amountRaven":
-                case "temperature":
-                    if (entry[1] != "") {
-                        recipe.innerHTML += "Temperature: " + entry[1] + " °C" + "<br>";
-                    }
-                    break;
-            }
+        var order = document.querySelector("div#order");
+        order.innerHTML = " ";
+        var formData = new FormData(document.ondragover[0]);
+        for (var _i = 0, FormData_1 = FormData; _i < FormData_1.length; _i++) {
+            var entry = FormData_1[_i];
+            var item = document.querySelector("[value='" + entry[1] + "']");
+            var price = Number(item.getAttribute("price"));
+            order.innerHTML += item.name + " € " + price;
         }
     }
-})(L03_Hexenkessel || (L03_Hexenkessel = {}));
-//# sourceMappingURL=script.js.map
+    //function handleChange(_event:Event):void {
+    //let form: HTMLDivElement=<HTMLDivElement>document.querySelector("div#form");
+    //form.innerHTML=" ";
+    //let formData: FormData = new FormData(document.forms[0]);
+    //for (let entry of formData) {
+    //let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1]+ "']");
+    //let price: number = Number(item.getAttribute("price"));
+    //form.innerHTML += item.name + "  € " + price;
+    //}
+})(L04_Hexenkessel || (L04_Hexenkessel = {}));
--- Lektion_03/Aufgabe/Hexenkessel.ts
+++ Lektion_03/Aufgabe/Hexenkessel.ts
@@ -4,22 +4,35 @@ namespace L04_Hexenkessel {
 function handleLoad(_event: Event): void {
     console.log("Start");
     let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+    let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order");
 
-    form.addEventListener("change", handleChange);
+    order.addEventListener("change", handleChange);
   
 }
-function handleChange(_event:Event):void {
-   
+function handleChange(_event:Event):void{
+    let order: HTMLDivElement=<HTMLDivElement>document.querySelector("div#order");
+    order.innerHTML=" ";
 
-let order: HTMLDivElement = <HTMLDivElement>document.querySelector("div#order");
-order.innerHTML = " ";
+    let formData: FormData = new FormData(document.ondragover[0]);
+    for (let entry of FormData){
+            let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1]+ "']");
+            let price: number = Number(item.getAttribute("price"));
 
-let formData: FormData = new FormData(document.forms[0]);
-for (let entry of formData) {
-    let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1]+ "']");
-    let price: number = Number(item.getAttribute("price"));
-   
-    order.innerHTML += item.name + "  € " + price;
+            order.innerHTML += item.name + " € " + price;
+    }
 }
+
+//function handleChange(_event:Event):void {
+ 
+//let form: HTMLDivElement=<HTMLDivElement>document.querySelector("div#form");
+//form.innerHTML=" ";
+
+//let formData: FormData = new FormData(document.forms[0]);
+//for (let entry of formData) {
+    //let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1]+ "']");
+    //let price: number = Number(item.getAttribute("price"));
+   
+    //form.innerHTML += item.name + "  € " + price;
+//}
 }
 
